9884494d76507d1c4d0efd46f92d182f8abd7422
Clara Gaya Saura <cgayasaura@innovatvie-technology.com>
2019-05-24 09:45:40 +0100

    Track auth status

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect} from 'react-redux';
+
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props;
+    console.log(auth)
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">PerritoDown</Link>
-                <SignedInLinks />
-                <SignedOutLinks />
+                { links }
             </div>
         </nav>                 
     )
 }
 
+const mapStateToProps = state => {
+    return {
+        auth: state.firebase.auth
+    }
+}
 
 
 
-
-export default Navbar;
+export default connect(mapStateToProps)(Navbar);
