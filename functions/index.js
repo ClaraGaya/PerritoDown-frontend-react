df4fe1425907e0da52db1f256ce6771c13315ab7
Clara Gaya Saura <cgayasaura@innovatvie-technology.com>
2019-05-29 13:17:18 +0100

    Add cloud Function Firestore and Auth Trigger

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,50 @@
+// This code will only run in the firebase server and not in the browser
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+// Initialize the app and pass it functions config
+// to be able to use the admin sdk to interact with different firebase services
+// like the auth service and the firestore service
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+// request this function to be run on a http request 
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello you!");
+});
+
+const createNotification = notification => {
+    return admin.firestore().collection('notifications')
+    .add(notification)
+    .then(doc => console.log('notification added', doc));
+}
+
+// This function will trigger when a new project is been created using firestore service
+exports.routineCreated = functions.firestore
+.document('routines/{routine}')
+.onCreate(doc => {
+    const routine = doc.data();
+    const notification = {
+        content: 'Added a new project',
+        user: `${routine.authorFirstName} ${routine.authorLastName}`,
+        uid: routine.authorId,
+        time: admin.firestore.FieldValue.serverTimestamp()
+    }
+    return createNotification(notification);
+});
+
+
+// This function will trigger when a user has been created using the auth service
+exports.userJoined = functions.auth.user()
+.onCreate(user => {
+    return admin.firestore().collection('users')
+    .doc(user.uid).get().then(doc => {
+        const newUser = doc.data();
+        const notification = {
+            content: 'Joined the party!',
+            user: `${newUser.firstName} ${newUser.lastName}`,
+            uid: user.uid,
+            time: admin.firestore.FieldValue.serverTimestamp()
+        }
+        return createNotification(notification); 
+    })
+})
